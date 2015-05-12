d92f7306851a332e1a12e4a1e26c6360742e9a7e
tarrenj <tarrenj@users.noreply.github.com>
2015-05-11 20:41:02 -0700

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,6 +1,6 @@
 // Initialize object stats
-enum direction(none = 0, left, down, up, right);
-enum collision(no = 0, enemy, loot, wall);
+enum DIRECTIONS(none = 0, left, down, up, right);
+enum COLLISIONS(no = 0, enemy, loot, wall);
 int x_pos, y_pos;
 int speed = 0;
 
