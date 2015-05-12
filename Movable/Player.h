49f4db964ee61d007b1033160efab5771e596a3a
tarrenj <tarrenj@users.noreply.github.com>
2015-05-11 20:42:39 -0700

    Update Player.h

--- Movable/Player.h
+++ Movable/Player.h
@@ -1,1 +1,10 @@
+#include "Person.h"
 
+class Player:public Movable{
+  public:
+    // Constructor and destructor
+    Player(int x_pos, int y_pos, DIRECTIONS direction, COLLISIONS collision, int speed, int health, int strength, int armor);
+    ~Player(void);
+  
+  vurtual draw(); // We'll deal with this later too...
+}
