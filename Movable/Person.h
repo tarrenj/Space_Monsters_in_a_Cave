a0ffec5e05cc0da5bee1a988b8c43f7e29523a1d
tarrenj <tarrenj@users.noreply.github.com>
2015-05-11 20:40:00 -0700

    Update Person.h

--- Movable/Person.h
+++ Movable/Person.h
@@ -1,1 +1,11 @@
+#include "Movable.h"
 
+class Person:public Movable{
+
+  public:
+    // Construcor and destructor
+    Person(int x_pos, int y_pos, DIRECTIONS direction, COLLISIONS collision, int speed, int health, int strength, int armor);
+    ~Person(void);
+    
+    virtual draw(); // We'll deal with this later...
+};
