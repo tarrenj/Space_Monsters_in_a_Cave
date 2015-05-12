ae8c462396011cc62aa0975541cff6c70e3c15eb
tarrenj <tarrenj@users.noreply.github.com>
2015-05-11 20:25:52 -0700

    Update Movable.h

--- Movable/Movable.h
+++ Movable/Movable.h
@@ -1,10 +1,7 @@
 #include "constants.h"
 
-// Setters and Getters
-void public get_health;
-void public get_strength;
-void public get_armor;
-
-// General methods
-void public update()
-void public collision()
+class Movable{
+  // General methods
+  void public update()
+  void public collision()
+};
